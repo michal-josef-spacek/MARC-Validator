294cdc79c8fc74ad3fa13da2a67235fc6f7db033
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-04-16 21:16:19 +0200

    First version

---
+++ Validator.pm
@@ -0,0 +1,12 @@
+package MARC::Validator;
+
+use strict;
+use warnings;
+
+use Module::Pluggable require => 1;
+
+our $VERSION = 0.01;
+
+1;
+
+__END__
---
+++ Validator/Plugin/Field008.pm
@@ -0,0 +1,62 @@
+package MARC::Validator::Plugin::Field008;
+
+use base qw(MARC::Validator::Abstract);
+use strict;
+use warnings;
+
+use English;
+use Error::Pure::Utils qw(err_msg_hr);
+use MARC::Leader;
+use MARC::Field008;
+
+our $VERSION = 0.01;
+
+sub name {
+	my $self = shift;
+
+	return 'field_008';
+}
+
+sub process {
+	my ($self, $marc_record) = @_;
+
+	my $leader_string = $marc_record->leader;
+	my $leader = MARC::Leader->new(
+		'verbose' => $self->{'verbose'},
+	)->parse($leader_string);
+
+	my $field_008_string = $marc_record->field('008')->as_string;
+	my $field_008 = eval {
+		MARC::Field008->new(
+			'leader' => $leader,
+			'verbose' => $self->{'verbose'},
+		)->parse($field_008_string);
+	};
+	if ($EVAL_ERROR) {
+		my $cnb = $marc_record->field('015')->subfield('a');
+		my $err = $EVAL_ERROR;
+		chomp $err;
+		my $err_msg_hr = err_msg_hr();
+		$self->{'struct'}->{'stats'}->{'not_valid'}->{$cnb} = {
+			'error' => $err,
+			'params' => $err_msg_hr,
+		};
+	}
+
+	return;
+}
+
+sub _init {
+	my $self = shift;
+
+	$self->{'struct'}->{'module_name'} = __PACKAGE__;
+	$self->{'struct'}->{'module_version'} = $VERSION;
+
+	$self->{'struct'}->{'stats'}->{'not_valid'} = {};
+
+	return;
+}
+
+1;
+
+__END__
