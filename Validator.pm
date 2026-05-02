f9cc5e45206667a1a7c1a2e70957eaf35f41f502
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-05-02 15:35:21 +0200

    Update output of EXAMPLE in doc

--- Validator.pm
+++ Validator.pm
@@ -57,10 +57,16 @@ Returns list of plugin module name strings.
  # List of plugins:
  # - MARC::Validator::Plugin::Field008
  # - MARC::Validator::Plugin::Field020
- # - MARC::Validator::Plugin::Field040
  # - MARC::Validator::Plugin::Field035
+ # - MARC::Validator::Plugin::Field040
+ # - MARC::Validator::Plugin::Field045
+ # - MARC::Validator::Plugin::Field080
  # - MARC::Validator::Plugin::Field260
  # - MARC::Validator::Plugin::Field264
+ # - MARC::Validator::Plugin::Field300
+ # - MARC::Validator::Plugin::Field500
+ # - MARC::Validator::Plugin::Field504
+ # - MARC::Validator::Plugin::Field655
 
 =head1 DEPENDENCIES
 
