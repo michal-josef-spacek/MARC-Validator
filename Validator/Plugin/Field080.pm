d31c17983735c9f5f3a25c75eed79ff454cf579d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-05-06 08:27:59 +0200

    Prepare for 0.22 version

--- Validator/Plugin/Field080.pm
+++ Validator/Plugin/Field080.pm
@@ -8,7 +8,7 @@ use Business::UDC 0.07;
 use Data::MARC::Validator::Report::Error 0.02;
 use Data::MARC::Validator::Report::Plugin::Errors 0.02;
 
-our $VERSION = 0.21;
+our $VERSION = 0.22;
 
 sub module_name {
 	my $self = shift;
